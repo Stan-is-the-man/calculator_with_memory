b3293a35a06639a65ef3c65d24d862129ccf11d2
Stanislav Yanakiev <stan5050@gmail.com>
2023-05-07 13:30:59 +0300

    'major changes and db added'

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
 import time
 import psycopg2
 
-connection = psycopg2.connect(
-    host='localhost',
-    database='calculator_with_memory',
-    user='postgres',
-    password='1123QwER'
-)
-
-cursor_object = connection.cursor()
-
-cursor_object.execute(
-    """CREATE TABLE IF NOT EXISTS calculator_statistics (    
-    action_type VARCHAR(10),
-    results FLOAT,   
-    execution_time FLOAT
-)"""
-)
-
-connection.commit()
-
 
 def basic_operators(first_number, second_number, action):
-    result = 0
     if action == "*":
         result = first_number * second_number
     elif action == "+":
@@ -31,10 +11,9 @@ def basic_operators(first_number, second_number, action):
         result = first_number - second_number
     elif action == "%":
         result = first_number % second_number
-
     elif action == '^':
         result = first_number ** second_number
-    elif action == '/':
+    else:
         result = first_number / second_number
     return result
 
@@ -55,15 +34,26 @@ def calculate_execution_time(func, *args):
 
 
 def data_to_db(the_action, the_result, the_total_time):
-    cursor_object.execute("""
-    INSERT INTO calculator_statistics(
-    action_type,
-    results,
-    execution_time
+    connection = psycopg2.connect(
+        host='localhost',
+        database='calculator_memory',
+        user='postgres',
+        password='1123QwER'
     )
+    cursor_object = connection.cursor()
+    cursor_object.execute(
+        """CREATE TABLE IF NOT EXISTS calculator_statistics (    
+        action_type VARCHAR(10),
+        results FLOAT,   
+        execution_time FLOAT
+    )"""
+    )
+    # connection.commit()
+
+    cursor_object.execute("""
+    INSERT INTO calculator_statistics(action_type, results, execution_time)
     VALUES(%s, %s, %s)
     """, (the_action, the_result, the_total_time))
-
     connection.commit()
 
 
@@ -73,46 +63,51 @@ while True:
     if operator == 'exit':
         break
     elif operator in ['+', '-', '*', '%']:
-        number_1 = float(input("Please enter the first number:\n"))
-        number_2 = float(input("Please enter the second number:\n"))
-        res = basic_operators(number_1, number_2, operator)
-        exec_time = calculate_execution_time(basic_operators, number_1, number_2, operator)
-        data_to_db(operator, res, exec_time)
-        print(res)
+        try:
+            number_1 = float(input("Please enter the first number:\n"))
+            number_2 = float(input("Please enter the second number:\n"))
+            res = basic_operators(number_1, number_2, operator)
+            exec_time = calculate_execution_time(basic_operators, number_1, number_2, operator)
+            data_to_db(operator, res, exec_time)
+            print(res)
+        except ValueError:
+            print('Only numbers accepted')
 
     elif operator == '/':
-        number_1 = float(input("Please enter the first number:\n"))
-        number_2 = float(input("Please enter the second number, other than ZERO:\n"))
         try:
-            if number_1 != 0 and number_2 != 0:
+            number_1 = float(input("Please enter the first number:\n"))
+            number_2 = float(input("Please enter the second number, other than ZERO:\n"))
+            if number_2 != 0:
                 res = basic_operators(number_1, number_2, operator)
                 exec_time = calculate_execution_time(basic_operators, number_1, number_2, operator)
                 data_to_db(operator, res, exec_time)
                 print(res)
-        except ZeroDivisionError:
-            print('Zero division not possible')
+            else:
+                print('Zero division not possible')
+        except ValueError:
+            print('Only numbers accepted')
 
     elif operator == "^":
         try:
-            number = int(input("Please enter an integer number:\n"))
+            number = float(input("Please enter any number:\n"))
             power = int(input("Please enter the power - integer number:\n"))
-            if isinstance(number, int) and isinstance(power, int):
+            if isinstance(power, int):
                 res = basic_operators(number, power, operator)
                 exec_time = calculate_execution_time(basic_operators, number, power, operator)
                 data_to_db(operator, res, exec_time)
                 print(res)
-
+            else:
+                print("Only integers numbers accepted for the power !!!")
         except ValueError:
-            print("Please enter only integers numbers for the power !!!\n")
+            print('Only numbers accepted')
 
     elif operator == "!":
         try:
             the_number = int(input("Please enter just ONE INTEGER number:\n"))
-            if isinstance(the_number, int):
-                res = factorial(the_number)
-                exec_time = calculate_execution_time(factorial, the_number)
-                data_to_db(operator, res, exec_time)
-                print(res)
+            res = factorial(the_number)
+            exec_time = calculate_execution_time(factorial, the_number)
+            data_to_db(operator, res, exec_time)
+            print(res)
         except ValueError:
             print("The entered number should be an integer !!!\n")
     else:
